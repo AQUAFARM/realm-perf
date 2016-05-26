fdc05980d4509b9eb973f85eddabcbd3ce87781e
yshrsmz <the.phantom.bane@gmail.com>
2016-05-26 19:57:14 +0900

    PojoActivity skelton

---
+++ app/src/main/java/net/yslibrary/realmperf/pojo/NoteDto.java
@@ -0,0 +1,14 @@
+package net.yslibrary.realmperf.pojo;
+
+/**
+ * Created by shimizu_yasuhiro on 2016/05/26.
+ */
+public class NoteDto {
+    public long id;
+
+    public String note;
+
+    public String note2;
+
+    public long createdAt;
+}
---
+++ app/src/main/java/net/yslibrary/realmperf/pojo/PojoActivity.java
@@ -0,0 +1,13 @@
+package net.yslibrary.realmperf.pojo;
+
+import net.yslibrary.realmperf.TestBaseActivity;
+
+import android.os.Bundle;
+
+public class PojoActivity extends TestBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
